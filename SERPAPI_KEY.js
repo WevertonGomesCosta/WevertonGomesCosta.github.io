986fb1b2659b04859d0dcb8befe29b23a48965cc
WevertonGomesCosta <weverton.costa@ufv.br>
2025-10-15 14:13:49 -0300

    delete serpapi_key

--- SERPAPI_KEY.js
+++
@@ -1,3 +0,0 @@
-// Este arquivo contém a chave da API. Em um ambiente de produção real,
-// esta chave NUNCA deve ser exposta no lado do cliente.
-const SERPAPI_KEY = "77282a568959bc1c31cdf88333aaab9768aa7d8ed9db90bd17e5db5a2f08c9f1";
